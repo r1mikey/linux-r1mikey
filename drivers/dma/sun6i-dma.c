98cd2e3992313eb1e9c99da0d793403830fe08b6
Andre Przywara <andre.przywara@arm.com>
2016-11-23 20:05:20 +0000

    dmaengine: add support for A64 DMA controller
    The A64 DMA controller is cut from the same cloth as the DMA controllers
    in the other (newer) Allwinner SoCs.
    So just add a new compatible with the respective settings in the driver.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- drivers/dma/sun6i-dma.c
+++ drivers/dma/sun6i-dma.c
@@ -1028,11 +1028,19 @@ static struct sun6i_dma_config sun8i_h3_dma_cfg = {
 	.nr_max_vchans   = 34,
 };
 
+static struct sun6i_dma_config sun50i_a64_dma_cfg = {
+	.nr_max_channels =  8,
+	.nr_max_requests = 27,
+	.nr_max_vchans   = 38,
+};
+
 static const struct of_device_id sun6i_dma_match[] = {
 	{ .compatible = "allwinner,sun6i-a31-dma", .data = &sun6i_a31_dma_cfg },
 	{ .compatible = "allwinner,sun8i-a23-dma", .data = &sun8i_a23_dma_cfg },
 	{ .compatible = "allwinner,sun8i-a83t-dma", .data = &sun8i_a83t_dma_cfg },
 	{ .compatible = "allwinner,sun8i-h3-dma", .data = &sun8i_h3_dma_cfg },
+	{ .compatible = "allwinner,sun50i-a64-dma",
+						.data = &sun50i_a64_dma_cfg },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, sun6i_dma_match);
